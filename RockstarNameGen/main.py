916163655aabad458e46bd5cf4eef44c5a94e8a4
hitechyeti <hitechyeti@gmail.com>
2024-12-06 00:44:02 -0500

    Rockstar Name Generator

---
+++ RockstarNameGen/main.py
@@ -0,0 +1,7 @@
+#Rockstar Name Generator
+print("Rockstar Name Generator!")
+
+street = input("What's the name of the street you grew up on? ")
+pet = input("What's the name of your first pet? ")
+
+print("Your ultimate rockstar name is: " + street + " " + pet)
